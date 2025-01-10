eb1f5562ddd4f8ae43a3af0ac9c5635adc7676a0
Tristan <tmfreiberg@gmail.com>
2025-01-10 15:56:37 -0500

    gemm testing_circuit.py added

--- python_testing/testing_circuit.py
+++ python_testing/testing_circuit.py
@@ -14,13 +14,17 @@ class BaseTests():
         #######################################################################################################
         '''
         # Specify
-        self.name = "testing"
+        self.name = "gemm"
         
         # Function input generation
 
-        self.inputs_1 = torch.randint(low=0, high=100, size=(256,))
-        self.inputs_2 = torch.randint(low=0, high=100, size=(256,))
+        self.input_a = torch.randint(low=0, high=100, size=(256,256))
+        self.input_b = torch.randint(low=0, high=100, size=(256,256))
+        self.input_c = torch.randint(low=0, high=100, size=(256,1))
+        self.input_alpha = torch.randint(low=0, high=100, size=(1,)).item()
+        self.input_beta = torch.randint(low=0, high=100, size=(1,)).item()
         # self.scaling = 100000000
+        
         '''
         #######################################################################################################
         #######################################################################################################
@@ -42,13 +46,29 @@ class BaseTests():
         '''
         ## Perform calculation here
 
-        outputs = torch.add(self.inputs_1,self.inputs_2)
+        def _gemm11(a, b, c, alpha, beta):  # type: ignore
+            o = np.dot(a.T, b.T) * alpha
+            if c is not None and beta != 0:
+                o += c * beta
+            return o
+
+        c_matrix = self.input_c.numpy() if self.input_c else None
+
+        outputs = _gemm11(self.input_a.numpy(), 
+                          self.input_b.numpy(), 
+                          c_matrix, 
+                          self.input_alpha, 
+                          self.input_beta) 
 
         ## Define inputs and outputs
         inputs = {
-            'inputs_1': [int(i) for i in self.inputs_1.tolist()],
-            'inputs_2': [int(i) for i in self.inputs_2.tolist()]
+            'input_a': [int(i) for i in self.input_a.tolist()],
+            'input_b': [int(i) for i in self.input_b.tolist()],          
+            'input_alpha': [int(i) for i in self.input_alpha.tolist()],
+            'input_beta': [int(i) for i in self.input_beta.tolist()],
             }
+        if c is not None:
+            inputs['input_c'] = [int(i) for i in self.input_c.tolist()],
         outputs = {
             'outputs': [int(i) for i in outputs.tolist()],
         }
