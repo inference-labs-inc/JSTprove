7f0b2d11aa1b87152b68be7c20ed3ef20210aaaf
jsgold-1 <jonathan.gold98@gmail.com>
2025-02-21 18:06:45 -0500

    Add convolution tests

--- python_testing/tests/test_conv.py
+++ python_testing/tests/test_conv.py
@@ -29,6 +29,7 @@ def test_convolution_base_quantize_run():
     test_circuit.base_testing(input_folder,proof_folder, temp_folder, weights_folder, circuit_folder, proof_system, output_folder)
 
 
+
 def test_convolution_incorrect_output():
     test_circuit = Convolution()
     with pytest.raises(subprocess.CalledProcessError) as exc_info:
@@ -45,20 +46,17 @@ def test_convolution_incorrect_output():
     assert expected_stdout in exc_info.value.stdout, f"Expected to find '{expected_stdout}' in stdout, but got: {exc_info.value.stdout}"
 
 
+def test_quantized_convolution_incorrect_output():
+    test_circuit = QuantizedConv()
+    with pytest.raises(subprocess.CalledProcessError) as exc_info:
+        with mock.patch.object(QuantizedConv, 'get_output', side_effect=convolution_incorrect_output, autospec=True) as mock_get_output:
+            test_circuit.base_testing(input_folder,proof_folder, temp_folder, weights_folder, circuit_folder, proof_system, output_folder)
 
-# if __name__ == "__main__":
-#     proof_system = ZKProofSystems.Expander
-#     proof_folder = "analysis"
-#     output_folder = "output"
-#     temp_folder = "temp"
-#     input_folder = "inputs"
-#     weights_folder = "weights"
-#     circuit_folder = ""
-#     #Rework inputs to function
-#     print("Testing Convolution")
-#     test_circuit = Convolution()
-#     test_circuit.base_testing(input_folder,proof_folder, temp_folder, weights_folder, circuit_folder, proof_system, output_folder)
+    assert exc_info is not None, "Expected subprocess.CalledProcessError to be raised, but it was not."
+    # Check stderr for the panic message or error message
+    error_message = "assertion `left == right` failed"
+    assert error_message in exc_info.value.stderr, f"Expected to find '{error_message}' in stderr, but got: {exc_info.value.stderr}"
 
-#     print("Testing Quantized Convolution")
-#     test_circuit = QuantizedConv()
-#     test_circuit.base_testing(input_folder,proof_folder, temp_folder, weights_folder, circuit_folder, proof_system, output_folder)
+    # Optionally check the stdout if necessary
+    expected_stdout = "built layered circuit"
+    assert expected_stdout in exc_info.value.stdout, f"Expected to find '{expected_stdout}' in stdout, but got: {exc_info.value.stdout}"
