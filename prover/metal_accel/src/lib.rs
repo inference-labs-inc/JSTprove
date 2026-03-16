a3fec2aa6a89a8ef1966b502b30ab443f48f5460
millioner <millioner.bbb@gmail.com>
2026-03-16 19:35:23 +0100

    Linux compiling fix

--- prover/metal_accel/src/lib.rs
+++ prover/metal_accel/src/lib.rs
@@ -1,11 +1,17 @@
 #![allow(clippy::all, clippy::pedantic)]
 
+#[cfg(target_os = "macos")]
 mod buffers;
+#[cfg(target_os = "macos")]
 mod device;
+#[cfg(target_os = "macos")]
 mod kernels;
 
+#[cfg(target_os = "macos")]
 pub use buffers::{MetalBufferPool, BN254_ELEM_SIZE};
+#[cfg(target_os = "macos")]
 pub use device::{MetalAccelerator, GPU_DISPATCH_THRESHOLD};
+#[cfg(target_os = "macos")]
 pub use kernels::{
     metal_cross_prod_eq, metal_eq_eval_at, metal_fold_all, metal_fold_f, metal_fold_hg,
     metal_poly_eval, metal_vec_add, BN254_R,
